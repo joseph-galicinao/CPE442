d533c20b42f721914e5a09e2a2a151f71f7adfd1
joseph <joseph@Xubuntu.myguest.virtualbox.org>
2024-09-30 10:20:39 -0700

    Added comments

--- lab2.cpp
+++ lab2.cpp
@@ -5,19 +5,22 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    // Case: valid number of arguments
     if (argc == 2) {
-        Mat image = imread(argv[1]);
-        
+        Mat image = imread(argv[1]); // Reads the image
+
+        // Case: image does not exist
         if (image.empty()) {
             std::cout << "Image does not exist\n";
             return -1;
         }
 
-        imshow("Image", image);
+        imshow("Image", image); // Displays the image
 
-        waitKey(0);
+        waitKey(0); // Waits to close the new tab before exitting
         return 0;
     }
+    // Case: invalid number of arguments (expects 1 argument)
     else {
         std::cout << "Invalid input\n";
         return -1;
